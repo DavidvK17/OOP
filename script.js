d34d06ba0450973d62dd0c8b66c01142ad7b3974
DavidvK17 <david.sing7@gmail.com>
2022-02-06 21:28:05 +0100

    Working with classes

--- script.js
+++ script.js
@@ -323,3 +323,30 @@ acc1
   .deposit(300)
   .deposit(500)
   .widthdraw(35);
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at  ${this.speed} km/h.`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h.`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EV extends CarCl {}
